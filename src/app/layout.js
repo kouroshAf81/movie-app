b82c27e390395ecc8b4fd6e1ed976c5f834ace4a
kourosh af <kouroshaf81@gmail.com>
2024-10-15 15:25:16 +0330

    add header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,18 @@
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import './globals.css';
+import Header from '../components/Header';
 
 export const metadata = {
-  title: "IMDB clone",
-  description: "movie database",
+    title: 'IMDB clone',
+    description: 'movie database'
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    );
 }
